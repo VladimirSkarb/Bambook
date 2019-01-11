33794aea80c6af67d2b6962d90a75399e6502d5b
Sergey <sergeyripchanskiy@gmail.com>
2019-01-11 11:49:50 +0200

    test for getting books from rails api

--- bambook-frontend/src/app/app.component.ts
+++ bambook-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bambook-frontend';
+  title = 'List of books from localhost:3000/books.json ';
+  books;
+
+  constructor(private http: HttpClient) {
+    http.get('http://localhost:3000/books.json')
+      .subscribe(res => this.books = res);
+  }
 }
