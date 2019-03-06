9a6ac5c527a5cb17a4126ed766ad30c92e0e43a9
westnick2 <vmetw@ukr.net>
2019-03-06 23:06:39 +0200

    Added post delete methods to offers controller

--- bambook_api/app/api/api_bambook/v1/offers.rb
+++ bambook_api/app/api/api_bambook/v1/offers.rb
@@ -3,8 +3,26 @@ module ApiBambook
     class Offers < Main
       resource :offers do
         get do
-          books = Offer.all
-          present books, with: ApiBambook::Entities::OffersEntity
+          offers = Offer.all
+          present offers, with: ApiBambook::Entities::OffersEntity
+        end
+
+        desc 'Create a new offer'
+        params do
+          requires :offer, type: Hash do
+            requires :deadline, type: Date
+            requires :link, type: String
+            requires :minimum_quantity, type: Integer
+          end
+        end
+        post do
+          authenticate!
+          offer = @current_user.offers.new(declared_params[:offer])
+          if offer.save
+            present :offer, offer, with: ApiBambook::Entities::OffersEntity
+          else
+            error!(offer.errors.messages, 422)
+          end
         end
 
         route_param :offer_id do
@@ -12,6 +30,13 @@ module ApiBambook
           get do
             offer = Offer.find(params[:offer_id])
             present offer, with: ApiBambook::Entities::OffersEntity
+          end
+
+          desc 'Delete offer'
+          delete do
+            authenticate!
+            @current_user.offers.find(params[:offer_id]).destroy
+            { status: :deleted }
           end
         end
       end
