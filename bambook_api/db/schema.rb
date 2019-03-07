1d4486e84c4bfcf4e0f15cfe2c2ffe7d6fa3b183
Sergey <sergeyripchanskiy@gmail.com>
2019-03-07 14:48:48 +0200

    add offer subscription model and dependencies

--- bambook_api/db/schema.rb
+++ bambook_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_224741) do
+ActiveRecord::Schema.define(version: 2019_03_07_123317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2019_03_05_224741) do
     t.string "cover_photo"
     t.string "book_file"
     t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
+  create_table "offer_subscriptions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "offer_id"
+    t.index ["offer_id"], name: "index_offer_subscriptions_on_offer_id"
+    t.index ["user_id"], name: "index_offer_subscriptions_on_user_id"
   end
 
   create_table "offers", force: :cascade do |t|
@@ -56,6 +65,8 @@ ActiveRecord::Schema.define(version: 2019_03_05_224741) do
   end
 
   add_foreign_key "books", "users"
+  add_foreign_key "offer_subscriptions", "offers"
+  add_foreign_key "offer_subscriptions", "users"
   add_foreign_key "offers", "users"
   add_foreign_key "reviews", "books"
   add_foreign_key "reviews", "users"
