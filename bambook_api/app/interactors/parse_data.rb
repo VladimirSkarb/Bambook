a892720c3019f55a992e01f966d2a3af4a2cebfc
Sergey <sergeyripchanskiy@gmail.com>
2019-03-11 21:52:04 +0200

    test fake yakaboo parser

--- bambook_api/app/interactors/parse_data.rb
+++ bambook_api/app/interactors/parse_data.rb
@@ -2,13 +2,13 @@ class ParseData
   include Interactor
 
   def call
-    # TODO: refactoring
-    # call parser interactor
+    site = context.params[:link]
 
-    # Fake data
-    context.params[:price] = 200
-    context.params[:avatar] = 'https://img.yakaboo.ua/media/catalog/product/cache/1/image/398x565/234c7c011ba026e66d29567e1be1d1f7/4/8/48280_78694.jpg'
-    context.params[:title] = 'Origin'
-    context.params[:author] = 'Dan Brown'
+    if site.include?('yakaboo')
+      YakabooParser.call(context)
+    else
+      context.errors = 'Undefined sourse'
+      context.fail!
+    end
   end
 end
