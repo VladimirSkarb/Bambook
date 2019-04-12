849b53722eac7fcd78fdfd9ea017dfb900033258
Sergey <sergeyripchanskiy@gmail.com>
2019-04-12 09:48:09 +0300

    overrade pagination for books component

--- bambook-frontend/src/app/components/books/books.component.ts
+++ bambook-frontend/src/app/components/books/books.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpParams} from '@angular/common/http';
-import {parseSelectorToR3Selector} from "@angular/compiler/src/core";
 import { PagerService } from '../../services/pager.service';
 
 @Component({
@@ -14,7 +13,6 @@ export class BooksComponent {
   books_count;
   current_page;
   pager: any = {};
-  pagedItems: any[];
 
   constructor(private http: HttpClient, private pagerService: PagerService ) {
     http.get('http://localhost:3000/api/v1/books').toPromise().then((resp) => {
@@ -28,8 +26,6 @@ export class BooksComponent {
       let params = new HttpParams({fromObject:{page: `${page}`}});
       return this.http.get(`http://localhost:3000/api/v1/books`, {params}).toPromise().then((resp) => {
         this.books = resp;
-        console.log(this.books.current_page)
-        console.log(this.books)
         this.setPage(page)
       });
     }
@@ -37,11 +33,5 @@ export class BooksComponent {
   setPage(page: number) {
     // get pager object from service
     this.pager = this.pagerService.getPager(this.books_count, page);
-    console.log(this.pager)
-
-    // get current page of items
-    this.pagedItems = this.books_count.slice(this.pager.startIndex, this.pager.endIndex + 1);
-    console.log(this.pagedItems)
-  }
-
+    }
   }
