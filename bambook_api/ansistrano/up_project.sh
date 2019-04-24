ad879651c88a64fad15a424790b09e5d177a2fea
Sergey <sergeyripchanskiy@gmail.com>
2019-04-24 20:12:43 +0300

    add and configure ansistrano

---
+++ bambook_api/ansistrano/up_project.sh
@@ -0,0 +1,10 @@
+cd /home/app/ma/current
+
+docker-compose -f docker-compose.production.yml stop
+docker-compose -f docker-compose.production.yml rm -f -v
+docker-compose -f docker-compose.production.yml build
+docker-compose -f docker-compose.production.yml run --rm rails rake db:migrate
+docker-compose -f docker-compose.production.yml stop
+docker-compose -f docker-compose.production.yml up -d
+sleep 5
+docker ps
